729e5890862b5bc65821615f42ab46e4e981bfb5
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-23 12:28:15 +0100

    init userRoute.js with folder images && route /api

--- api/server.js
+++ api/server.js
@@ -7,6 +7,10 @@ const express = require('express');
 const app = express();
 const port = process.env.SERVER_PORT | 3000;
 
+const userRoute = require('./routes/userRoute');
+
+app.use('/api', userRoute);
+
 app.listen(port, function(){
     console.log('Server Listen on port'+port);
 });
