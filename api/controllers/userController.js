9397018b6d042da6c13bd68b72dc024df2cdfdf4
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-24 18:16:33 +0100

    send mail verif Otp

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const Blacklist = require('../models/blacklist');
+const Otp = require('../models/otp');
 const { validationResult } = require('express-validator');
 const mailer = require('../helpers/mailer');
 const randomstring = require('randomstring');
@@ -444,6 +445,68 @@ const logout = async(req, res) => {
     }
 }
 
+const generateRandom4Digit = async() => {
+    return Math.floor(1000 + Math.random() * 9000);
+}
+
+const sendOtp = async(req, res) => {
+    try{
+
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                success: false,
+                msg: 'Errors',
+                errors: errors.array()
+            });
+        }
+
+        const { email } = req.body;
+
+        const userData = await User.findOne({ email });
+
+        if(!userData){
+            return res.status(400).json({
+                success: false,
+                msg: "Email doesn't exists!"
+            });
+        }
+
+        if(userData.is_verified == 1){
+            return res.status(400).json({
+                success: false,
+                msg: userData.email+" mail is already verified!"
+            });
+        }
+
+        const g_otp = await generateRandom4Digit();
+
+        const enter_otp = new Otp({
+            user_id: userData._id,
+            otp: g_otp
+        });
+
+        await enter_otp.save();
+
+        const msg = '<p> Hii <b>'+userData.name+'</b>, </br> <h4>'+g_otp+'</h4></p>';
+
+        mailer.sendMail(userData.email, 'Otp Verification', msg);
+
+        return res.status(200).json({
+            success: true,
+            msg: 'Otp has been sent to your mail, please check!'
+        });
+
+
+    } catch(error){
+        return res.status(400).json({
+            success: false,
+            msg: error.message
+        });
+    }
+}
+
 module.exports = {
     userRegistre,
     mailVerification,
@@ -456,5 +519,6 @@ module.exports = {
     userProfile,
     updateProfile,
     refreshToken,
-    logout
+    logout,
+    sendOtp
 }
