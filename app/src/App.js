a67118d6e4d5b364035f8b7088258399d127a1b5
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-25 18:51:19 +0100

    dashboard

--- app/src/App.js
+++ app/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={ <Navigate to="/login" /> } />
+
         <Route path='/login' Component={Login} />
         <Route path='/register' Component={Register} />
+
+        <Route path='/dashboard' Component={Dashboard} />
       </Routes>
     </Router>
     
